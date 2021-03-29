28ffc08ea8e5dcbaf02b5c99c67becba654e7c88
Johnny Cockburn <ccie15385@gmail.com>
2021-03-30 10:00:51 +1300

    change password

--- initialize_fabric.py
+++ initialize_fabric.py
@@ -14,7 +14,7 @@ apic_ip ='10.103.23.10'
 username = 'admin'
 #f = open('credentials', 'r')
 #switchpassword = f.readline()
-password = 'Avpnmw3wl!'
+password = 'secret'
 #Excel File name
 file_name = 'fabric_init.xlsx'
 
